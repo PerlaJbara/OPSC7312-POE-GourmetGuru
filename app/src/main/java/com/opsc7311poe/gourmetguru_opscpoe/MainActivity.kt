43e4e541b1a26d10b3cd3661b425db5b87b687cf
ST10022447 Perla Jbara <st10022447@vcconnect.edu.za>
2024-08-27 17:10:03 +0200

    Nav Bar working. Home Page UI coded and functioning. Home page requires a few tweaks.

---
+++ app/src/main/java/com/opsc7311poe/gourmetguru_opscpoe/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.opsc7311poe.gourmetguru_opscpoe
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        bottomNavView = findViewById(R.id.bottom_navigation)
+
+        replaceFragment(HomeFragment())
+        bottomNavView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navHome -> {
+                    Log.d("MainActivity", "Home selected")
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.navMealPlan -> {
+                    Log.d("MainActivity", "Meal Plan selected")
+                    replaceFragment(MealPlanFragment())
+                    true
+                }
+                R.id.navSearch -> {
+                    Log.d("MainActivity", "Search selected")
+                    replaceFragment(SearchFragment())
+                    true
+                }
+                R.id.navMyProfile -> {
+                    Log.d("MainActivity", "My Profile selected")
+                    replaceFragment(MyProfileFragment())
+                    true
+                }
+                R.id.navMyRecipe -> {
+                    Log.d("MainActivity", "My Recipe selected")
+                    replaceFragment(MyRecipesFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // Set default fragment if there's no saved instance state
+        if (savedInstanceState == null) {
+            replaceFragment(HomeFragment())
+
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        Log.d("MainActivity", "Replacing fragment: ${fragment::class.java.simpleName}")
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_container, fragment)
+            .commit()
+    }
+}
