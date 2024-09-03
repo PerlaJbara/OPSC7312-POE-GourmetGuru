a03fc1f8b628219483d68b63b76685944e36e128
ST10022447 Perla Jbara <st10022447@vcconnect.edu.za>
2024-09-03 14:43:37 +0200

    Started and completed: login screen, initial splash screen, registration screen, lebanese fragment, italian fragment,mexican fragment, portuguese fragment, french fragment, japanese fragment, sa fragment and indian fragment. Added navigation between all screens.

--- app/src/main/java/com/opsc7311poe/gourmetguru_opscpoe/HomeFragment.kt
+++ app/src/main/java/com/opsc7311poe/gourmetguru_opscpoe/HomeFragment.kt
@@ -6,21 +6,77 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 
 class HomeFragment : Fragment() {
 
+    private lateinit var btnLeb: ImageView
+    private lateinit var btnItalian: ImageView
+    private lateinit var btnMexican: ImageView
+    private lateinit var btnPortu: ImageView
+    private lateinit var btnFrench: ImageView
+    private lateinit var btnJapan: ImageView
+    private lateinit var btnSA: ImageView
+    private lateinit var btnIndian: ImageView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.d("HomeFragment", "onCreateView")
-        // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d("HomeFragment", "onViewCreated called")
-        // Initialization code here
+
+        btnLeb = view.findViewById(R.id.btnLebaneseCuisine)
+        btnItalian = view.findViewById(R.id.btnItalianCuisine)
+        btnMexican = view.findViewById(R.id.btnMexicanCuisine)
+        btnPortu = view.findViewById(R.id.btnPortCuisine)
+        btnFrench = view.findViewById(R.id.btnFrenchCuisine)
+        btnJapan = view.findViewById(R.id.btnJapaneseCuisine)
+        btnSA = view.findViewById(R.id.btnSACuisine)
+        btnIndian = view.findViewById(R.id.btnIndianCuisine)
+
+        btnLeb.setOnClickListener() {
+            replaceFragment(LebaneseFragment())
+        }
+
+        btnItalian.setOnClickListener(){
+            replaceFragment(ItalianFragment())
+        }
+
+        btnMexican.setOnClickListener(){
+            replaceFragment(MexicanFragment())
+        }
+
+        btnPortu.setOnClickListener(){
+            replaceFragment(PortugueseFragment())
+        }
+
+        btnFrench.setOnClickListener(){
+            replaceFragment(FrenchFragment())
+        }
+
+        btnJapan.setOnClickListener(){
+            replaceFragment(JapaneseFragment())
+        }
+
+        btnSA.setOnClickListener(){
+            replaceFragment(SouthafricanFragment())
+        }
+
+        btnIndian.setOnClickListener(){
+            replaceFragment(IndianFragment())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.frame_container, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
