0a46480e358571e3c3c50847e9b16c48389e0dbc
GabbiJ <gabriellajanssen9@gmail.com>
2024-09-05 16:40:13 +0200

    Added functionality to back arrow on SearchFragment

--- app/src/main/java/com/opsc7311poe/gourmetguru_opscpoe/SearchFragment.kt
+++ app/src/main/java/com/opsc7311poe/gourmetguru_opscpoe/SearchFragment.kt
@@ -1,23 +1,43 @@
 package com.opsc7311poe.gourmetguru_opscpoe
 
 import android.os.Bundle
+import android.util.Log
+import android.view.HapticFeedbackConstants
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 
 class SearchFragment : Fragment() {
 
+    private lateinit var btnBack: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_search, container, false)
+
+        btnBack = view.findViewById(R.id.btnBack)
+
+        btnBack.setOnClickListener(){
+            it.performHapticFeedback(HapticFeedbackConstants.CONFIRM)
+            replaceFragment((HomeFragment()))
+        }
+
+        return view
     }
 
+    private fun replaceFragment(fragment: Fragment) {
+        Log.d("ServicesFragment", "Replacing fragment: ${fragment::class.java.simpleName}")
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.frame_container, fragment)
+            .commit()
+    }
 
 
 }
