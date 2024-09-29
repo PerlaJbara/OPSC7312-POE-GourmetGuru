6e4bd23cfdac0718cd8f2de4cf7e472122272183
ST10034968 <ST10034968@vcconnect.edu.za>
2024-09-29 10:59:18 +0200

    my profile page now displays user info

--- app/src/main/java/com/opsc7311poe/gourmetguru_opscpoe/MyProfileFragment.kt
+++ app/src/main/java/com/opsc7311poe/gourmetguru_opscpoe/MyProfileFragment.kt
@@ -7,11 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
 
 
 class MyProfileFragment : Fragment() {
 
 private lateinit var btnOpenSettings: ImageView
+private lateinit var txtName: TextView
+private lateinit var txtEmail: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,8 +31,29 @@ private lateinit var btnOpenSettings: ImageView
         val view = inflater.inflate(R.layout.fragment_my_profile, container, false)
 
         //fetching and displaying user information
-        
+        txtName = view.findViewById(R.id.txtUsername)
+        txtEmail = view.findViewById(R.id.txtEmail)
 
+        val database = Firebase.database
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        val userRef = database.getReference("Users").child(userId!!)
+        if (userId != null) {
+
+            userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                    txtName.text = dataSnapshot.child("name").getValue().toString() + " " + dataSnapshot.child("surname").getValue().toString()
+                    txtEmail.text = dataSnapshot.child("email").getValue().toString()
+
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    Toast.makeText(requireContext(), "Error reading from the database: ${databaseError.message}", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+
+        //handling setting button navigation
         btnOpenSettings = view.findViewById(R.id.btnsettings)
 
         btnOpenSettings.setOnClickListener(){
